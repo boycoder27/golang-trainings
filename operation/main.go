574b834c4126c2ae61e73b72c2f995d8ae3d0585
Jhon Louis Alcantara <jhnlslcntr27@gmail.com>
2025-08-22 18:32:27 +0800

    added comment on codes

--- operation/main.go
+++ operation/main.go
@@ -6,11 +6,13 @@ func main() {
 	num1 := 10
 	num2 := 5
 
+	// start operations
 	sum := num1 + num2
 	dif := num1 - num2
 	product := num1 * num2
 	quotient := num1 / num2
 
+	// display output
 	fmt.Printf("The sum is %d\n", sum)
 	fmt.Printf("The dif is %d\n", dif)
 	fmt.Printf("The product is %d\n", product)
