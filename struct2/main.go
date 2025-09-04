61e6ad81d5027952bd5b5e4c64852e1197c13bfc
Jhon Louis Alcantara <jhnlslcntr27@gmail.com>
2025-09-04 19:29:20 +0800

    Added struct2

---
+++ struct2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type book struct {
+	book         string
+	author       string
+	yearReleased int
+	borrowed     bool
+}
+
+func main() {
+	book1 := book{
+		book:         "Noli Me Tangere",
+		author:       "Dr.Jose Rizal",
+		yearReleased: 1887,
+		borrowed:     true,
+	}
+
+	book2 := book{
+		book:         "Cinderella",
+		author:       "Charles Perrault",
+		yearReleased: 1697,
+		borrowed:     false,
+	}
+
+	book3 := book{
+		book:         "Superman",
+		author:       "James Gunn",
+		yearReleased: 2026,
+		borrowed:     false,
+	}
+
+	fmt.Printf("Book of %s, The author is %s, The Year Released is %d, is this book borrowed? %t\n", book1.book, book1.author, book1.yearReleased, book1.borrowed)
+	fmt.Printf("Book of %s, The author is %s, The Year Released is %d, is this book borrowed? %t\n", book2.book, book2.author, book2.yearReleased, book2.borrowed)
+	fmt.Printf("Book of %s, The author is %s, The Year Released is %d, is this book borrowed? %t\n", book3.book, book3.author, book3.yearReleased, book3.borrowed)
+
+}
