a17233ba8612d2de8d0b2809427b1434f28cc232
Jhon Louis Alcantara <jhnlslcntr27@gmail.com>
2025-09-09 20:08:14 +0800

    Added array_struct2

---
+++ array_struct2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Customer struct {
+	Name   string
+	Gender string
+}
+
+func main() {
+
+	customers := []Customer{
+		{
+			Name:   "Marie",
+			Gender: "Female",
+		},
+		{
+			Name:   "Richard",
+			Gender: "Male",
+		},
+		{
+			Name:   "Dingdong",
+			Gender: "Male",
+		},
+		{
+			Name:   "Rose",
+			Gender: "Female",
+		},
+		{
+			Name:   "Carla",
+			Gender: "Female",
+		},
+	}
+	for _, customer := range customers {
+		if customer.Gender == "Male" {
+			fmt.Printf("Hi Sir %s welcome\n", customer.Name)
+		} else {
+			fmt.Printf("Hi Maam %s welcome\n", customer.Name)
+		}
+
+	}
+}
