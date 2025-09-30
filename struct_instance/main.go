0a982f446c72df48f07fe65ae6a6f6860b003d97
Jhon Louis Alcantara <jhnlslcntr27@gmail.com>
2025-10-01 00:53:57 +0800

    Initial commit

--- struct_instance/main.go
+++ struct_instance/main.go
@@ -21,13 +21,30 @@ func main() {
 	addition := NewMath(10, 5, "Add")
 	sum := addition.Compute()
 	fmt.Println("Sum is:", sum)
+
+	substract := NewMath(25, 5, "Sub")
+	diff := substract.Compute()
+	fmt.Println("Diff is:", diff)
+
+	multiplication := NewMath(3, 3, "Multiply")
+	product := multiplication.Compute()
+	fmt.Println("Multiply is:", product)
+
+	quotient := NewMath(100, 10, "Division")
+	division := quotient.Compute()
+	fmt.Println("Division is:", division)
 }
 
 func (m Math) Compute() int {
 	var answer int
 	if m.Operation == "Add" {
 		answer = m.FirstNumber + m.SecondNumber
+	} else if m.Operation == "Sub" {
+		answer = m.FirstNumber - m.SecondNumber
+	} else if m.Operation == "Multiply" {
+		answer = m.FirstNumber * m.SecondNumber
+	} else if m.Operation == "Division" {
+		answer = m.FirstNumber / m.SecondNumber
 	}
-
 	return answer
 }
